eec23ddd1b48e4e23f44d590032f0eefe09a3478
Alexander <alexyack@users.noreply.github.com>
2021-08-04 16:31:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace AssemblyTester
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if(args.Count() == 0)
+			{
+				Console.Error.WriteLine("Empty command-line.");
+				return 0;
+			}
+
+			int classCount = 0;
+
+			try
+			{
+				Assembly asm = Assembly.LoadFile(Path.GetFullPath(args[0]));
+
+				IEnumerable<TypeInfo> types = asm.DefinedTypes;
+
+				foreach(TypeInfo ti in types)
+				{
+					if(ti.IsClass)
+					{
+						Console.Error.WriteLine("Found class '{0}'.", ti.Name);
+						classCount++;
+					}
+				}
+
+				/*
+				classCount = Assembly.LoadFile(Path.GetFullPath(args[0])).DefinedTypes.Select(ti => ti.IsClass).Count();
+				*/
+			}
+			catch(BadImageFormatException)
+			{
+				Console.Error.WriteLine("Assembly '{0}' is not loadable.", args[0]);
+			}
+			catch(Exception e)
+			{
+				Console.Error.WriteLine("Assembly '{0}' throws exception {1}.", args[0], e.Message);
+			}
+
+			Console.WriteLine("Assembly '{1}', Number of classes: {0}", classCount, args[0]);
+
+			return classCount;
+		}
+	}
+}
